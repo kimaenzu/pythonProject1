a942027b744aa1a0ecb6edcba04632dccce20380
kimaenzu <khj5606kh@gmail.com>
2022-11-08 11:14:06 +0900

    cm2

--- Calc.py
+++ Calc.py
@@ -3,6 +3,8 @@ def add_func (n1, n2):
 
 def min_func (n1, n2):
     return n1-n2
+def mul_func (n1, n2):
+    return n1*n2
 
 n1, n2, res = 100, 200, 0
 
@@ -10,3 +12,5 @@ res = add_func(n1, n2)
 print(n1, "+", n2, "=", res)
 res = min_func(n1, n2)
 print(n1, "-", n2, "=", res)
+res = mul_func(n1, n2)
+print(n1, "*", n2, "=", res)
